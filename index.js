388758d7d5575e321834a51b2e1ac0450f98ed5d
Fitz <fitz.wen@edragonconnect.com>
2020-05-13 10:34:08 +0800

    update

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+import "regenerator-runtime";
 import { BrowserBarcodeReader } from "@zxing/library/esm";
-console.log("BrowserBarcodeReader", BrowserBarcodeReader);
+function setup() {
+  const codeReader = new BrowserBarcodeReader();
+
+  let selectedDeviceId;
+  codeReader
+    .getVideoInputDevices()
+    .then((videoInputDevices) => {
+      selectedDeviceId = videoInputDevices[0].deviceId;
+      document.getElementById("startButton").addEventListener("click", () => {
+        codeReader
+          .decodeOnceFromVideoDevice(selectedDeviceId, "video")
+          .then((result) => {
+            console.log(result);
+            codeReader.reset();
+            document.getElementById("result").textContent = result.text;
+          })
+          .catch((err) => {
+            console.error(err);
+            document.getElementById("result").textContent = err;
+            codeReader.reset();
+          });
+        console.log(
+          `Started continous decode from camera with id ${selectedDeviceId}`
+        );
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+window.addEventListener("load", () => setup());
