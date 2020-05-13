67a8baf33cac8f02d0e22c0c0ecc915c746d8bde
Fitz <fitz.wen@edragonconnect.com>
2020-05-13 10:50:31 +0800

    udpate

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 import "regenerator-runtime";
+import "webrtc-adapter";
 import { BrowserBarcodeReader } from "@zxing/library/esm";
 function setup() {
   const codeReader = new BrowserBarcodeReader();
-
-  navigator.mediaDevices.getUserMedia({
-    video: { facingMode: "environment" },
-  });
-  let selectedDeviceId;
-  codeReader
-    .getVideoInputDevices()
-    .then((videoInputDevices) => {
-      selectedDeviceId = videoInputDevices[0].deviceId;
-
-      document.getElementById("startButton").addEventListener("click", () => {
-        alert("startbutton");
-        codeReader
-          .decodeOnceFromVideoDevice(selectedDeviceId, "video")
-          .then((result) => {
-            console.log(result);
-            codeReader.reset();
-            document.getElementById("result").textContent = result.text;
-          })
-          .catch((err) => {
-            console.error(err);
-            document.getElementById("result").textContent = err;
-            codeReader.reset();
-          });
-        console.log(
-          `Started continous decode from camera with id ${selectedDeviceId}`
-        );
+  document.getElementById("startButton").addEventListener("click", () => {
+    codeReader
+      .decodeOnceFromConstraints(
+        {
+          video: { facingMode: "environment", width: 320, height: 160 },
+        },
+        "video"
+      )
+      .then((result) => {
+        console.log(result);
+        document.getElementById("result").textContent = result.text;
+      })
+      .catch((err) => {
+        console.error(err);
+        document.getElementById("result").textContent = err;
       });
-    })
-    .catch((err) => {
-      alert(err.message);
-      console.error(err);
-    });
+  });
 }
 
 window.addEventListener("load", () => setup());
